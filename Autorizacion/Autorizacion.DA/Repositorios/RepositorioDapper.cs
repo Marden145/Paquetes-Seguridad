ad4a3334a1128f6bc6f34486e9b179e7e62a949b
Marden145 <moragafallas@gmail.com>
2025-06-29 16:07:04 -0600

    paquetes middleware

---
+++ Autorizacion/Autorizacion.DA/Repositorios/RepositorioDapper.cs
@@ -0,0 +1,23 @@
+﻿using Autorizacion.Abstracciones.DA;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace Autorizacion.DA.Repositorios
+{
+    public class RepositorioDapper : IRepositorioDapper
+    {
+        private readonly IConfiguration _configuration;
+        private SqlConnection _connection;
+
+        public RepositorioDapper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connection = new SqlConnection(_configuration.GetConnectionString("BDSeguridad"));
+        }
+
+        public SqlConnection ObtenerRepositorioDapper()
+        {
+            return _connection;
+        }
+    }
+}
---
+++ Autorizacion/Autorizacion.DA/SeguridadDA.cs
@@ -0,0 +1,35 @@
+﻿using Autorizacion.Abstracciones.DA;
+using Autorizacion.Abstracciones.Modelos;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using Helpers;
+
+namespace Autorizacion.DA
+{
+    public class SeguridadDA : ISeguridadDA
+    {
+        IRepositorioDapper _repositorioDapper;
+        private SqlConnection _SqlConnection;
+
+        public SeguridadDA(IRepositorioDapper repositorioDapper)
+        {
+            _repositorioDapper = repositorioDapper;
+            _SqlConnection = repositorioDapper.ObtenerRepositorioDapper();
+        }
+
+        public async Task<IEnumerable<Perfiles>> ObtenerPerfilesUsuario(Usuario usuario)
+        {
+            string sql = @"[ObtenerPerfilesxUsuario]";
+            var consulta= await _SqlConnection.QueryAsync<Abstracciones.Entidades.Perfiles>(sql, new { CorreoElectronico=usuario.CorreoElectronico ,NombreUsuario=usuario.NombreUsuario });
+            return Convertidor.ConvertirLista<Abstracciones.Entidades.Perfiles, Abstracciones.Modelos.Perfiles>(consulta);
+        }
+
+        public async Task<Usuario> ObtenerUsuario(Usuario usuario)
+        {
+            string sql = @"[ObtenerUsuario]";
+            var consulta = await _SqlConnection.QueryAsync<Abstracciones.Entidades.Usuario>(sql, new { CorreoElectronico = usuario.CorreoElectronico
+                , NombreUsuario = usuario.NombreUsuario });
+            return Convertidor.Convertir<Abstracciones.Entidades.Usuario, Abstracciones.Modelos.Usuario>(consulta.FirstOrDefault());
+        }
+    }
+}
